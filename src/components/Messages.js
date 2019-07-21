1fa2642e52fba60ec12af6ced5bc8ddab62552d6
ecimio <edson.cimionatto@nike.com>
2019-07-20 17:58:52 -0700

    setting and printing the user

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,29 +1,23 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
+import {useAuth0} from "../react-auth0-wrapper";
 
 const API = 'http://localhost:8080/';
 const DEFAULT_QUERY = 'xcomm';
 
-class Messages extends Component {
-    constructor(props) {
-        super(props);
+function Messages(props) {
 
-        this.state = {
-            communications: [],
-        };
-    }
+    const [communications, setCommunications] = useState([]);
+    const {user} = useAuth0();
 
-    componentWillMount() {
-        this.fetchMessages()
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchMessages()
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
-        this.interval = setInterval(() => this.fetchMessages(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    fetchMessages() {
+    const fetchMessages = () => {
         fetch(API + DEFAULT_QUERY, {
             crossDomain: true,
             method: 'GET',
@@ -31,43 +25,44 @@ class Messages extends Component {
         })
             .then(res => res.json())
             .then((data) => {
-                this.setState({communications: data})
+                setCommunications(data)
             })
             .catch(console.log)
     }
 
-    deleteMessage(id) {
+    const deleteMessage = id => {
         fetch(API + DEFAULT_QUERY + "/" + id, {
             crossDomain: true,
             method: 'DELETE',
             headers: {'Content-Type': 'application/json'},
+            body: {'user': user.email}
         }).catch(console.log)
     }
 
-    render() {
-        return (
-            <div className="container">
-                <h3 className="modal-title">Messages</h3>
-                <p>
-                    <Link className="btn btn-info" to="/newMessage">New</Link>
-                </p>
-                {this.state.communications.length === 0 ? "no records" : this.state.communications.map(message => (
-                    <div className="card">
-                        <h5 className="card-header">{message.id} - {message.status}</h5>
-                        <div className="card-body">
-                            <p className="card-text">{message.scheduledTime}</p>
-                            <p className="card-text">emailTo: {message.emailTo ? message.emailTo : "N/A"}</p>
-                            <p className="card-text">slackTo: {message.slackTo ? message.slackTo : "N/A"}</p>
-                            <p className="card-text">{message.message}</p>
-                            <button className="btn btn-danger" value={message.id}
-                                    onClick={() => this.deleteMessage(message.id)}>remove
-                            </button>
-                        </div>
+    return (
+        <div className="container">
+            <h3 className="modal-title">Messages</h3>
+            <p>
+                <Link className="btn btn-info" to="/newMessage">New</Link>
+            </p>
+            {communications.length === 0 ? "no records" : communications.map(message => (
+                <div className="card">
+                    <h5 className="card-header">{message.id} - {message.status}</h5>
+                    <div className="card-body">
+                        <p className="card-text">{message.scheduledTime}</p>
+                        <p className="card-text">emailTo: {message.emailTo ? message.emailTo : "N/A"}</p>
+                        <p className="card-text">slackTo: {message.slackTo ? message.slackTo : "N/A"}</p>
+                        <p className="card-text">{message.message}</p>
+                        <p className="card-text">created by: {message.user}</p>
+                        <button className="btn btn-danger" value={message.id}
+                                onClick={() => deleteMessage(message.id)}>remove
+                        </button>
                     </div>
-                ))}
-            </div>
-        )
-    }
+                </div>
+            ))}
+        </div>
+    )
+
 
 }
 
