7c8378ce158de9a8c3b8323fc8ea70f34e1c6e59
ecimio <edson.cimionatto@nike.com>
2019-08-11 20:15:11 -0700

    moving to k8

--- minikubeStart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-kubectl create -f ./kubMongoVolume.yaml
-kubectl create -f ./kubMongoVolumeClaim.yaml
-kubectl create -f ./kubMongo.yaml
-kubectl create -f ./kubMongoService.yaml
-kubectl create -f ./backend.yaml
-kubectl create -f ./frontend.yaml
-kubectl get services
-minikube ip
